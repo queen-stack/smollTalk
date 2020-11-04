51ffd2d0dc6a7541689722e4708f38f906ff97ee
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-03 18:25:58 -0700

    made signup and login functionality

--- cypress/integration/tests/signingInloggingIn.test.js
+++ cypress/integration/tests/signingInloggingIn.test.js
@@ -15,6 +15,23 @@ class loggingIn {
         cy.get('#loginBtn').click();
     }
 
+    static signup() {
+        // Verifies the Username input field
+        cy.get('#signup-username-input').type('usernameTest');
+        // Verifies user input inside the "Email" field
+        cy.get('#signup-email-input').type('emailTest@emailTest.com');
+        // Verifies user input inside the "Password" field
+        cy.get('#signup-password-input').type('passwordTest');
+        // Verifying user input inside the "LinkedIn" field
+        cy.get('#signup-linkedin-input').type('https://www.linkedin.com/in/unavailable/');
+        // Verifying user input inside the "Github" field
+        cy.get('#signup-github-input').type('https://github.com/');
+        // Verifying user input inside the "Twitter" field
+        cy.get('#signup-twitter-input').type('https://twitter.com/?lang=en');
+        // Clicks the signup button in the modal to make a user
+        cy.get('#signupModalBtn').click();
+    }
+
     static login() {
         // Verifies user input inside the "Username" field
         cy.get('#login-username-input').type('tea');
